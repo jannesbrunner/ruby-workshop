2e3c5b9c564224db545199db22d8a34398374606
jannesbrunner <mail@jannebrunner.de>
2017-08-14 14:12:49 +0200

    Add rectangle exercise

---
+++ 2_rectangle/rectangle_test.rb
@@ -0,0 +1,9 @@
+require "minitest/autorun"
+require_relative "rectangle"
+
+class RectangleTest < Minitest::Test
+  def test_area
+    assert_equal 15, Rectangle.new(5, 3).area
+    assert_equal 400, Rectangle.new(200, 2).area
+  end
+end
